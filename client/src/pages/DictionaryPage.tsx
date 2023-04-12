0260eea67d5d7746f3142a2b6c60d8f49dd67746
Vinicius Nogueira <vns5218@icloud.com>
2023-04-12 16:58:13 -0300

    add dictionary words

--- client/src/pages/DictionaryPage.tsx
+++ client/src/pages/DictionaryPage.tsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 function DictionaryPage() {
+
+  interface Word {
+    _id: string;
+    Dutch: string;
+    English: string;
+  }
+
+const [words, setWords] = useState<Word[]>([]);
+
+let renderedWords  = words.map((word) => {
+  return  <div className="flex flex-row gap-2 pb-5"  key={word._id}>
+  <h3>{word.Dutch}</h3>
+  <h3>{word.English}</h3>
+</div>
+})
+
+useEffect(() => {
+  const fetchData = async () => {
+    const res = await axios.get('http://localhost:3000/words')
+    setWords(res.data)
+  }
+  fetchData()
+}, [])
+
   return (
-    <div>DictionaryPage</div>
+    <div>
+      <div className="flex flex-row gap-2 justify-center items-center" >
+        <h1>Dutch</h1>
+        <h1>English</h1>
+      </div>
+    <div className="flex flex-col justify-center items-center" >{renderedWords}</div>
+    </div>
   )
 }
 
