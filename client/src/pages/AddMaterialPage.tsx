479a63347c99f2aa2a344f8aeace5c14600c7f0a
Vinicius Nogueira <vns5218@icloud.com>
2023-04-25 16:16:28 -0300

    add post method and page

--- client/src/pages/AddMaterialPage.tsx
+++ client/src/pages/AddMaterialPage.tsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import { useState } from 'react'
+import axios from 'axios'
 
 function AddMaterialPage() {
+const [content, setContent] = useState<String>('')
+const [title, setTitle] = useState<String>('')
+
+
+const handleContentChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+ setContent(event.target.value)
+}
+
+
+const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+ setTitle(event.target.value)
+}
+
+
+const addPost = (event: any) => {
+  event.preventDefault()
+  let newPost = { title: title, text: content }
+  axios.post('http://localhost:3000/contentpost', newPost)
+}
+
+
+
   return (
-    <div>AddMaterialPage</div>
+    <div>
+
+<form className="flex flex-col justify-start items-start " >
+
+<h1 className="text-2xl text-blue-flag" >Title</h1>
+<input className=" w-full h-9 " onChange={handleTitleChange}  />
+
+<h1 className="text-2xl text-blue-flag mt-10" >Content</h1>
+<textarea maxLength={1200} className=" w-full " onChange={handleContentChange} ></textarea>
+
+<button className=" ml-2 text-2xl bg-blue-flag text-white rounded-sm px-2 py-1 mt-10 " onClick={addPost} >Post</button>
+
+</form>
+
+    </div>
   )
 }
 
