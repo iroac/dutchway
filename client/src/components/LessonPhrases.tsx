9d161c7261f1d8c875b6ba79e2b970ffd3dacd40
Vinicius Nogueira <vns5218@icloud.com>
2023-05-28 13:00:09 -0300

    add new question logic in LessonPhrases

--- client/src/components/LessonPhrases.tsx
+++ client/src/components/LessonPhrases.tsx
@@ -12,7 +12,7 @@ function LeassonPhrases() {
   // Fetching States
   const [pointUser, setPointUser] = useState<User | undefined>(user)
   const [options, setOptions] = useState<Phrases[]>([]);
-  const [questions, setQuestions] = useState<Word[]>([]);
+  const [questions, setQuestions] = useState<Phrases[]>([]);
   // Question/Total States
   const [score, setScore] = useState<number>(0);
   const [showFinalResult, setShowFinalResult] = useState<boolean>(false);
@@ -58,15 +58,16 @@ axios.put(`http://localhost:3000/users/${user?.id}`, pointUser)
   }
 
   const handleCheckButton = () => {
-    let hasEnglish = questions[currentQuestionIndex].phrases.some((phrase) => phrase.english === text);
-    let hasDutch = questions[currentQuestionIndex].phrases.some((phrase) => phrase.dutch === text);
+    let hasEnglish = questions[currentQuestionIndex].english === text;
+    let hasDutch = questions[currentQuestionIndex].dutch === text;
 if (order === 'eng' ? hasEnglish : hasDutch ) {
 setCheckButton(false);
 setContinueButton(true);
 setSelectAnswerRight(true);
 setScore(score + 1)
-let questId = questions[currentQuestionIndex].id
-const questIndex = pointUser?.currentlyWords.findIndex((innerArray: number[]) => innerArray[0] === questId);
+const actualPhrase = questions[currentQuestionIndex].english
+const foundObject = currentlyWords.find((obj) => obj.phrases.find((phrase) => phrase.english === actualPhrase))
+const questIndex = pointUser?.currentlyWords.findIndex((innerArray: number[]) => innerArray[0] === foundObject?.id);
 if (questIndex !== undefined && questIndex !== -1 && pointUser) {
   pointUser.currentlyWords[questIndex][1] += 1;
 }
@@ -87,7 +88,6 @@ setTotalQuestions(totalQuestions + 1)
     return shuffledArray;
   };
 
-
   const shuffledOptions = (array: Phrases[]) => {
     const shuffledArray = [...array];
     for (let i = shuffledArray.length - 1; i > 0; i--) {
@@ -104,11 +104,12 @@ setTotalQuestions(totalQuestions + 1)
     if (currentlyWords.length > 0) {
       let shuffledQuestions = shuffleArray(currentlyWords).slice(0, 2);
       let hasPhrases = shuffledQuestions.some((quest) => quest.phrases[1].english === '');
+      console.log(currentlyWords)
       while(hasPhrases) {
         shuffledQuestions = shuffleArray(currentlyWords).slice(0, 2);
         hasPhrases = shuffledQuestions.some((quest) => quest.phrases[1].english === '');
       }
-      setQuestions(shuffledQuestions);
+      setQuestions([shuffledQuestions[0].phrases[0], shuffledQuestions[0].phrases[1], shuffledQuestions[1].phrases[0], shuffledQuestions[1].phrases[1]]);
       setOptions([shuffledQuestions[0].phrases[0], shuffledQuestions[0].phrases[1], shuffledQuestions[1].phrases[0], shuffledQuestions[1].phrases[1]]);
     }
   }, []);
@@ -119,8 +120,8 @@ setTotalQuestions(totalQuestions + 1)
       <div className="flex flex-col justify-center items-center mt-16 ">
         {questions.length > 0 && !showFinalResult && (
           <>
-          { order === 'dutch' ?  <div className="flex flex-row mx-auto shadow-md h-52 w-auto justify-center bg-blue-flag text-white text-3xl items-center cursor-pointer mb-12">{questions[currentQuestionIndex].phrases[0].english.split(' ').map((word, index) => (<span key={index} className={`${word.toLowerCase() === questions[currentQuestionIndex].english.toLowerCase() ? 'text-red-500' : ''} mr-2 `}>{word} </span>))}</div> : ""}
-          { order === 'eng' ?  <div className="flex flex-row mx-auto shadow-md h-52 w-auto justify-center bg-blue-flag text-white text-3xl items-center cursor-pointer mb-12">{questions[currentQuestionIndex].phrases[0].dutch.split(' ').map((word, index) => (<span key={index} className={`${word.toLowerCase()  === questions[currentQuestionIndex].dutch.toLowerCase()  ? 'text-red-500' : ''} mr-2 `}>{word} </span>))}</div> : ""}
+          { order === 'dutch' ?  <div className="flex flex-row mx-auto shadow-md h-52 w-auto justify-center bg-blue-flag text-white text-3xl items-center cursor-pointer mb-12">{questions[currentQuestionIndex].english.split(' ').map((word, index) => (<span key={index} className={`${ currentlyWords.some((obj) => obj.english === word) ? 'text-red-500' : ''} mr-2 `}>{word} </span>))}</div> : ""}
+          { order === 'eng' ?  <div className="flex flex-row mx-auto shadow-md h-52 w-auto justify-center bg-blue-flag text-white text-3xl items-center cursor-pointer mb-12">{questions[currentQuestionIndex].dutch.split(' ').map((word, index) => (<span key={index} className={`${currentlyWords.some((obj) => obj.dutch === word) ? 'text-red-500' : ''} mr-2 `}>{word} </span>))}</div> : ""}
  
             {options.map((quest, index) => ( 
               <div
