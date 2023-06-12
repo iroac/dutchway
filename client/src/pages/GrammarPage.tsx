79c71fe59e95c10afdb9183e5af4d141cf2e931b
Vinicius Nogueira <vns5218@icloud.com>
2023-06-12 11:46:40 -0300

    change tailwind properties on Grammar and Material page

--- client/src/pages/GrammarPage.tsx
+++ client/src/pages/GrammarPage.tsx
@@ -4,31 +4,31 @@ function GrammarPage() {
   return (
     <div className={'flex flex-col h-screen w-screen justify-start items-center mt-20'} >
       <h1 className={'text-4xl text-blue-flag'}>Verb Tenses</h1>
-<div className={'flex flex-row w-full justify-center items-center gap-20 mt-10'} >
 
-  <div className='flex flex-col justify-center items-center ' >
+
+<div className={'flex flex-row w-11/12 justify-center items-center gap-5 sm:gap-20 mt-10'} >
+  <div className='flex flex-col justify-center items-center w-1/3 ' >
 <Link to="/grammar/past" style={{backgroundImage: `url(/utils/svg/past.jpg)`}} className={'flex rounded-full text-3xl w-40 h-40 text-white items-center justify-center bg-contain bg-no-repeat bg-center border-solid border-4 border-blue-flag hover:border-2'}></Link>
 <h1 className="text-blue-flag text-3xl" >PAST</h1>
 </div> 
 
-  <div className='flex flex-col justify-center items-center ' >
+  <div className='flex flex-col justify-center items-center w-1/3 ' >
 <Link to="/grammar/present" style={{backgroundImage: `url(/utils/svg/present.jpg)`}} className={' flex rounded-full text-3xl w-40 h-40 text-white items-center justify-center bg-contain bg-no-repeat bg-center border-solid border-4 border-red-flag hover:border-2 '}></Link>
 <h1 className="text-red-flag text-3xl" >PRESENT</h1>
 </div>
 
-  <div className='flex flex-col justify-center items-center ' >
+  <div className='flex flex-col justify-center items-center w-1/3 ' >
 <Link to="/grammar/future" style={{backgroundImage: `url(/utils/svg/future.jpg)`}} className={' flex rounded-full text-3xl w-40 h-40 text-white items-center justify-center bg-contain bg-no-repeat bg-center border-solid border-4 border-blue-flag hover:border-2'}></Link>
 <h1 className="text-blue-flag text-3xl" >FUTURE</h1>
 </div> 
-
-
 </div>
+
 <hr className='border-2 border-blue-flag w-11/12 mt-10'/>
 <h1 className={'text-4xl text-blue-flag mt-10'}>Structure</h1>
-<div className={'flex flex-row w-full justify-center items-center gap-20 mt-10'} >
-<Link to='/grammar/structure/adjectives' className={' flex rounded-full text-2xl w-40 h-40 text-white bg-blue-flag items-center justify-center'}>ADJECTIVES</Link>
-<Link to='/grammar/structure/modalverbs' className={' flex rounded-full text-1xl w-40 h-40 text-white bg-blue-flag items-center justify-center'}>MODAL VERBS</Link>
-<Link to='/grammar/structure/articles' className={' flex rounded-full text-2xl w-40 h-40 text-white bg-blue-flag items-center justify-center'}>ARTICLES</Link>
+<div className={'flex flex-row w-full justify-center items-center mt-10'} >
+<Link to='/grammar/structure/adjectives' className={' flex items-center justify-center w-1/3 '}> <div className="flex text-2xl w-40 rounded-full h-40 text-white bg-blue-flag items-center justify-center">ADJECTIVES</div> </Link>
+<Link to='/grammar/structure/modalverbs' className={' flex items-center justify-center w-1/3 '}> <div  className="flex text-2xl w-40 rounded-full h-40 text-white bg-blue-flag items-center justify-center text-center " >MODAL VERBS</div> </Link>
+<Link to='/grammar/structure/articles' className={' flex  items-center justify-center w-1/3'}><div className="flex text-2xl w-40 rounded-full h-40 text-white bg-blue-flag items-center  justify-center">ARTICLES</div></Link>
 </div>
     </div> 
   )
--- client/src/pages/MaterialPage.tsx
+++ client/src/pages/MaterialPage.tsx
@@ -5,10 +5,10 @@ import { Link } from "react-router-dom";
 
 function MaterialPage() {
   return (
-    <div className="flex flex-row justify-center items-center h-screen w-screen " > 
-<Link to="/posts" className=" flex flex-col mx-auto group shadow-md h-52 w-52 justify-center items-center cursor-pointer rounded-xl  hover:shadow-red-flag hover:bg-red-flag text-xl text-red-flag hover:text-white hover:rounded-2xl"> <BiBookBookmark className="text-8xl text-red-flag group-hover:text-white" />TEXT</Link>
-<Link to="/videos" className="flex flex-col mx-auto group shadow-md h-52 w-52 justify-center items-center cursor-pointer rounded-xl hover:shadow-blue-flag hover:bg-blue-flag text-xl text-blue-flag hover:text-white hover:rounded-2xl " ><RxVideo className="text-8xl text-blue-flag group-hover:text-white" /> VIDEOS </Link>
-<Link to="/addmaterial" className=" flex flex-col mx-auto group shadow-md h-52 w-52 justify-center items-center cursor-pointer rounded-xl hover:shadow-red-flag hover:bg-red-flag text-xl text-red-flag hover:text-white hover:rounded-2xl" > <AiOutlineAppstoreAdd className="text-8xl text-red-flag group-hover:text-white" />ADD</Link>
+    <div className="flex flex-col justify-center items-center h-screen w-screen bg-daily-gradient "> 
+<Link to="/posts" className=" flex flex-col group shadow-md mt-4 h-52 bg-white w-11/12 justify-center items-center cursor-pointer rounded-xl  hover:shadow-red-flag hover:bg-red-flag text-xl text-red-flag hover:text-white hover:rounded-2xl"> <BiBookBookmark className="text-8xl text-red-flag group-hover:text-white" />TEXT</Link>
+<Link to="/videos" className="flex flex-col group shadow-md mt-4 h-52 bg-white w-11/12 justify-center items-center cursor-pointer rounded-xl hover:shadow-blue-flag hover:bg-blue-flag text-xl text-blue-flag hover:text-white hover:rounded-2xl " ><RxVideo className="text-8xl text-blue-flag group-hover:text-white" /> VIDEOS </Link>
+<Link to="/addmaterial" className=" flex flex-col group shadow-md mt-4 h-52 bg-white w-11/12 justify-center items-center cursor-pointer rounded-xl hover:shadow-red-flag hover:bg-red-flag text-xl text-red-flag hover:text-white hover:rounded-2xl" > <AiOutlineAppstoreAdd className="text-8xl text-red-flag group-hover:text-white" />ADD</Link>
     </div>
   )
 }
